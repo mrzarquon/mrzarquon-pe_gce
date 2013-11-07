5386a215ffe8b287d8fad88c98c890fb9a5f17fe
Chris Barker <cbarker@puppetlabs.com>
2013-11-06 17:39:21 -0800

    initial commit

---
+++ files/site.pp
@@ -0,0 +1,23 @@
+ite.pp ##
+
+## Active Configurations ##
+
+# PRIMARY FILEBUCKET
+# This configures puppet agent and puppet inspect to back up file contents when
+# they run. The Puppet Enterprise console needs this to display file contents
+# and differences.
+
+# Define filebucket 'main':
+filebucket { 'main':
+  server => 'puppetmaster-1a.c.flowing-flame-390.internal',
+  path   => false,
+}
+
+# Make filebucket 'main' the default backup location for all File resources:
+File { backup => 'main' }
+
+# DEFAULT NODE
+
+node default {
+  include pe_mcollective
+}
---
+++ manifests/init.pp
@@ -0,0 +1,10 @@
+class pe_gce {
+  file { '/etc/puppetlabs/puppet/autosign.conf':
+    ensure  => file,
+    content => '*',
+  }
+  file { '/etc/puppetlabs/puppet/manifests/site.pp':
+    ensure => file,
+    source => 'puppet:///modules/pe_gce/site.pp',
+  }
+}
